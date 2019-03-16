6f0a3aead38a7d24a70eed8ae126668bf9daff7c
Federico Pintaluba <federico.pintaluba@tarmac.io>
2019-03-16 18:22:49 -0300

    Starter project in Kotlin.

---
+++ app/src/main/java/com/federicopintaluba/kotlinstarterkit/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.federicopintaluba.kotlinstarterkit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
